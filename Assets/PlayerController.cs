b0400d1f2f7fcabcc5ab6bbb24a5886a0c9194a6
Nail Sharipov <sharipovn@gmail.com>
2022-07-29 13:31:55 +0500

    lesson 1.2 - 5 - Customize the vehicle’s speed;

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -14,6 +14,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Move the vehicle forward
-        transform.Translate(Vector3.forward);
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
     }
 }
