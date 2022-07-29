6d9880e18ff9cf1cfff788c4b3d03f6d108f43e2
Nail Sharipov <sharipovn@gmail.com>
2022-07-29 18:00:54 +0500

    lesson 1.3 - 3 - Add an offset to the camera position;

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -15,6 +15,6 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position;
+        transform.position = player.transform.position + new Vector3(0, 5, -7);
     }
 }
